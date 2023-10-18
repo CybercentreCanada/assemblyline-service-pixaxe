5263f25708dff647b94a753dc7f54570f599d5bf
cccs-kevin <kevin.hardy-cooper@cyber.gc.ca>
2023-10-18 15:37:10 +0000

    Black formatting

--- pixaxe/pixaxe.py
+++ pixaxe/pixaxe.py
@@ -6,8 +6,8 @@ import subprocess
 from tempfile import NamedTemporaryFile
 
 import magic
-from assemblyline.odm.base import FULL_URI
 from assemblyline.common.str_utils import safe_str
+from assemblyline.odm.base import FULL_URI
 from assemblyline_v4_service.common.base import ServiceBase
 from assemblyline_v4_service.common.result import (
     Result,
@@ -30,11 +30,11 @@ ImageFile.LOAD_TRUNCATED_IMAGES = True
 class Pixaxe(ServiceBase):
     PAT_FILEMARKERS = {
         # Header, Trailer, additional methods
-        'bmp': ['\x42\x4D', None, 'bmp_dump'],
-        'gif': ['\x47\x49\x46\x38.\x61.{19,}\x2C.{9,}', '\x00\x3B', None],
-        'jpeg': ['\xFF\xD8.{18}\xFF\xDB.{3,}\xFF\xDA.{13,}', '\xFF\xD9', None],
-        'jpeg2000': ['\x00\x00\x00\x0C\x6A\x50\x20\x20\x0D\x0A', None, 'jp2_dump'],
-        'png': ['\x89\x50\x4E\x47', '\x49\x45\x4E\x44.{4}', None],
+        "bmp": ["\x42\x4D", None, "bmp_dump"],
+        "gif": ["\x47\x49\x46\x38.\x61.{19,}\x2C.{9,}", "\x00\x3B", None],
+        "jpeg": ["\xFF\xD8.{18}\xFF\xDB.{3,}\xFF\xDA.{13,}", "\xFF\xD9", None],
+        "jpeg2000": ["\x00\x00\x00\x0C\x6A\x50\x20\x20\x0D\x0A", None, "jp2_dump"],
+        "png": ["\x89\x50\x4E\x47", "\x49\x45\x4E\x44.{4}", None],
     }
 
     def __init__(self, config=None):
@@ -73,9 +73,9 @@ class Pixaxe(ServiceBase):
         # noinspection PyBroadException
         try:
             # Byte offset to start of image
-            soi = struct.unpack('<I', data[10:14])[0]
+            soi = struct.unpack("<I", data[10:14])[0]
             # Size of image data, including padding -- potentially unreliable
-            sizei = struct.unpack('<I', data[34:38])[0]
+            sizei = struct.unpack("<I", data[34:38])[0]
             # Width in pixels
             # wi = struct.unpack('<I', data[18:22])[0]
             # Height in pixels
@@ -86,8 +86,8 @@ class Pixaxe(ServiceBase):
             # bpi = struct.unpack('<H', data[28:30])[0]
             # Image bytes
             # sizei = (wi*hi*di*bpi)/8
-            bmp_data = data[0:(soi + sizei)]
-            verify_bmp = self.mimetype(bmp_data, 'image')
+            bmp_data = data[0 : (soi + sizei)]
+            verify_bmp = self.mimetype(bmp_data, "image")
             if not verify_bmp:
                 return data
             return bmp_data
@@ -104,13 +104,13 @@ class Pixaxe(ServiceBase):
             JPEG 2000 data if discovered, or original data.
         """
         ftyps = {
-            '\x6a\x70\x32\x20': 'jp2',
-            '\x6a\x70\x78\x20': 'jpf',
-            '\x6a\x70\x6d\x20': 'jpm',
-            '\x6d\x6a\x70\x32': 'mj2',
-            '\xFF\x4F\xFF\x51': 'j2c'
+            "\x6a\x70\x32\x20": "jp2",
+            "\x6a\x70\x78\x20": "jpf",
+            "\x6a\x70\x6d\x20": "jpm",
+            "\x6d\x6a\x70\x32": "mj2",
+            "\xFF\x4F\xFF\x51": "j2c",
         }
-        trailer = '\xFF\xD9'
+        trailer = "\xFF\xD9"
         cdata = data
         end = 0
         try:
@@ -127,13 +127,19 @@ class Pixaxe(ServiceBase):
                 else:
                     end += findend + 2
                 # Another jp2 codestream
-                if cdata[findend + 6:findend + 10] == 'jp2c':
-                    cdata = cdata[findend + 2:]
+                if cdata[findend + 6 : findend + 10] == "jp2c":
+                    cdata = cdata[findend + 2 :]
                 # Possible .mov file types
-                elif file_type == 'mj2' and cdata[findend + 6:findend + 10] in \
-                        ['free', 'mdat', 'moov', 'pnot', 'skip', 'wide']:
-                    msize = struct.unpack('>I', cdata[findend + 2:findend + 6])[0]
-                    jp2_data = data[0:end + msize]
+                elif file_type == "mj2" and cdata[findend + 6 : findend + 10] in [
+                    "free",
+                    "mdat",
+                    "moov",
+                    "pnot",
+                    "skip",
+                    "wide",
+                ]:
+                    msize = struct.unpack(">I", cdata[findend + 2 : findend + 6])[0]
+                    jp2_data = data[0 : end + msize]
                     break
                 else:
                     jp2_data = data[0:end]
@@ -151,15 +157,14 @@ class Pixaxe(ServiceBase):
         Returns:
             Embedded file data if found, or None.
         """
-        with open(alfile, 'rb') as f:
+        with open(alfile, "rb") as f:
             data = f.read()
         for _, tinfo in iter(self.PAT_FILEMARKERS.items()):
-
             # Build up the regex
             if tinfo[1] is not None:
-                embed_regex = re.compile(tinfo[0] + '.+' + tinfo[1], re.DOTALL)
+                embed_regex = re.compile(tinfo[0] + ".+" + tinfo[1], re.DOTALL)
             else:
-                embed_regex = re.compile(tinfo[0] + '.+', re.DOTALL)
+                embed_regex = re.compile(tinfo[0] + ".+", re.DOTALL)
 
             # Find the pattern that should match the image.
             img_match = re.match(embed_regex, str(data))
@@ -173,22 +178,22 @@ class Pixaxe(ServiceBase):
                 leftovers = data.replace(img_data, b"")
 
                 # Remove trailing NULL bytes
-                leftovers = re.sub(b'[\x00]*$', b'', leftovers)
+                leftovers = re.sub(b"[\x00]*$", b"", leftovers)
 
                 if len(leftovers) > 15:
                     return leftovers
         return
 
     def _analyseGifImage(self, path):
-        '''
+        """
         Pre-process pass over the image to determine the mode (full or additive).
         Necessary as assessing single frames isn't reliable. Need to know the mode
         before processing all frames.
-        '''
+        """
         im = PILImage.open(path)
         results = {
-            'size': im.size,
-            'mode': 'full',
+            "size": im.size,
+            "mode": "full",
         }
         try:
             while True:
@@ -197,7 +202,7 @@ class Pixaxe(ServiceBase):
                     update_region = tile[1]
                     update_region_dimensions = update_region[2:]
                     if update_region_dimensions != im.size:
-                        results['mode'] = 'partial'
+                        results["mode"] = "partial"
                         break
                 im.seek(im.tell() + 1)
         except EOFError:
@@ -205,44 +210,49 @@ class Pixaxe(ServiceBase):
         return results
 
     def _writeGifFrames(self, request, image_preview, ocr_heuristic_id, _handle_ocr_output):
-        '''
+        """
         Iterate the GIF, extracting each frame.
-        '''
-        mode = self._analyseGifImage(request.file_path)['mode']
+        """
+        mode = self._analyseGifImage(request.file_path)["mode"]
 
         im = PILImage.open(request.file_path)
 
         i = 0
         p = im.getpalette()
-        last_frame = im.convert('RGBA')
+        last_frame = im.convert("RGBA")
 
         try:
             while True:
-                '''
+                """
                 If the GIF uses local colour tables, each frame will have its own palette.
                 If not, we need to apply the global palette to the new frame.
-                '''
+                """
                 if p is not None and not im.getpalette() and im.mode in ("L", "LA", "P", "PA"):
                     im.putpalette(p)
 
-                new_frame = PILImage.new('RGBA', im.size)
+                new_frame = PILImage.new("RGBA", im.size)
 
-                '''
+                """
                 Is this file a "partial"-mode GIF where frames update a region of a different size to the entire image?
                 If so, we need to construct the new frame by pasting it on top of the preceding frames.
-                '''
-                if mode == 'partial':
+                """
+                if mode == "partial":
                     new_frame.paste(last_frame)
 
-                new_frame.paste(im, (0, 0), im.convert('RGBA'))
-                fh = NamedTemporaryFile(delete=False, suffix='.png')
-                new_frame.save(fh.name, 'PNG')
+                new_frame.paste(im, (0, 0), im.convert("RGBA"))
+                fh = NamedTemporaryFile(delete=False, suffix=".png")
+                new_frame.save(fh.name, "PNG")
                 fh.flush()
 
-                ocr_io = NamedTemporaryFile('w', delete=False)
+                ocr_io = NamedTemporaryFile("w", delete=False)
 
-                image_preview.add_image(fh.name, name=f"{request.file_name}_frame_{i}", description='GIF frame',
-                                        ocr_heuristic_id=ocr_heuristic_id, ocr_io=ocr_io)
+                image_preview.add_image(
+                    fh.name,
+                    name=f"{request.file_name}_frame_{i}",
+                    description="GIF frame",
+                    ocr_heuristic_id=ocr_heuristic_id,
+                    ocr_io=ocr_io,
+                )
                 _handle_ocr_output(ocr_io, fn_prefix=f"{request.file_name}_frame_{i}")
 
                 i += 1
@@ -257,30 +267,28 @@ class Pixaxe(ServiceBase):
 
         displayable_image_path = request.file_path
         pillow_incompatible = False
-        save_ocr_output = request.get_param('save_ocr_output').lower()
+        save_ocr_output = request.get_param("save_ocr_output").lower()
 
         def _handle_ocr_output(ocr_io, fn_prefix):
             # Write OCR output as specified by submissions params
-            if save_ocr_output == 'no':
+            if save_ocr_output == "no":
                 return
             else:
                 # Write content to disk to be uploaded
-                if save_ocr_output == 'as_extracted':
-                    request.add_extracted(ocr_io.name, f'{fn_prefix}_ocr_output',
-                                          description="OCR Output")
-                elif save_ocr_output == 'as_supplementary':
-                    request.add_supplementary(ocr_io.name, f'{fn_prefix}_ocr_output',
-                                              description="OCR Output")
+                if save_ocr_output == "as_extracted":
+                    request.add_extracted(ocr_io.name, f"{fn_prefix}_ocr_output", description="OCR Output")
+                elif save_ocr_output == "as_supplementary":
+                    request.add_supplementary(ocr_io.name, f"{fn_prefix}_ocr_output", description="OCR Output")
                 else:
-                    self.log.warning(f'Unknown save method for OCR given: {save_ocr_output}')
+                    self.log.warning(f"Unknown save method for OCR given: {save_ocr_output}")
 
-        if request.file_type.split('/')[-1] in ['svg', 'wmf', 'emf']:
+        if request.file_type.split("/")[-1] in ["svg", "wmf", "emf"]:
             try:
                 displayable_image_path = os.path.join(self.working_directory, f"{request.file_name}.png")
-                if any([request.file_type.endswith(wmf_type) for wmf_type in ['wmf', 'emf']]):
+                if any([request.file_type.endswith(wmf_type) for wmf_type in ["wmf", "emf"]]):
                     # PIL is able to identify WMF but not able to render, therefore we need to convert
                     Image(filename=request.file_path).save(filename=displayable_image_path)
-                elif request.file_type.endswith('svg'):
+                elif request.file_type.endswith("svg"):
                     # PIL doesn't support SVG so we will need to convert
                     svg2png(bytestring=request.file_contents, write_to=displayable_image_path)
 
@@ -299,9 +307,14 @@ class Pixaxe(ServiceBase):
                 self._writeGifFrames(request, image_preview, ocr_heuristic_id, _handle_ocr_output)
 
             else:
-                ocr_io = NamedTemporaryFile('w', delete=False)
-                image_preview.add_image(displayable_image_path, name=request.file_name, description='Input file',
-                                        ocr_heuristic_id=ocr_heuristic_id, ocr_io=ocr_io)
+                ocr_io = NamedTemporaryFile("w", delete=False)
+                image_preview.add_image(
+                    displayable_image_path,
+                    name=request.file_name,
+                    description="Input file",
+                    ocr_heuristic_id=ocr_heuristic_id,
+                    ocr_io=ocr_io,
+                )
                 _handle_ocr_output(ocr_io, fn_prefix=request.file_name)
             image_preview.promote_as_screenshot()
             result.add_section(image_preview)
@@ -310,7 +323,7 @@ class Pixaxe(ServiceBase):
             for i, decoded_qr in enumerate(qr_decode(PILImage.open(displayable_image_path))):
                 if re.match(FULL_URI, decoded_qr.data.decode()):
                     # Tag URI
-                    image_preview.add_tag('network.static.uri', decoded_qr.data)
+                    image_preview.add_tag("network.static.uri", decoded_qr.data)
                 else:
                     # Write data to file
                     fh = NamedTemporaryFile(delete=False, mode="wb")
@@ -328,26 +341,28 @@ class Pixaxe(ServiceBase):
         # Attempt to extract files from the image
         extract_path = NamedTemporaryFile(delete=False)
         p = subprocess.run(
-            ['stegseek', '-a', '-f', request.file_path, '/opt/al_service/rockyou.txt', extract_path.name],
-            capture_output=True).stderr
+            ["stegseek", "-a", "-f", request.file_path, "/opt/al_service/rockyou.txt", extract_path.name],
+            capture_output=True,
+        ).stderr
 
-        if b'Extracting to' in p:
-            self.log.info('Embedded file extracted from image.')
+        if b"Extracting to" in p:
+            self.log.info("Embedded file extracted from image.")
             extracted_section = ResultKeyValueSection("Secret file was extracted from image", parent=steg_section)
             lines = [x for x in p.decode().splitlines() if "e: " in x]
             orig_name = "steg_extract.txt"
             passphrase = None
             for line in lines:
                 if 'filename: "' in line:
-                    orig_name = line.split('filename: "')[1][:-2]         # Original filename: "Hello.txt".
+                    orig_name = line.split('filename: "')[1][:-2]  # Original filename: "Hello.txt".
                 if 'passphrase: "' in line:
                     passphrase = line.split('passphrase: "')[1].strip('"')  # Found passphrase: "pass"
 
             extracted_section.set_item("original_name", orig_name)
             if passphrase is not None:
                 extracted_section.set_item("passphrase", passphrase)
-            request.add_extracted(extract_path.name, orig_name, 'File extracted from image',
-                                  safelist_interface=self.api_interface)
+            request.add_extracted(
+                extract_path.name, orig_name, "File extracted from image", safelist_interface=self.api_interface
+            )
             extracted_section.set_heuristic(2)
 
         if pillow_incompatible:
@@ -359,7 +374,7 @@ class Pixaxe(ServiceBase):
         if "RGB" not in PILImage.open(request.file_path).mode:
             # Library expects an image containing RGB channels
             secret_msg = None
-        elif not request.file_type.endswith('jpg') or request.deep_scan:
+        elif not request.file_type.endswith("jpg") or request.deep_scan:
             try:
                 secret_msg = lsb.reveal(request.file_path)
             except IndexError:
@@ -367,8 +382,8 @@ class Pixaxe(ServiceBase):
                 pass
         # Think it's unlikely to have both a hidden message and an embedded file
         if secret_msg:
-            self.log.info('Secret message found.')
-            steg_section.set_body(f'Secret message found:\n{secret_msg}')
+            self.log.info("Secret message found.")
+            steg_section.set_body(f"Secret message found:\n{secret_msg}")
             steg_section.set_heuristic(2)
 
         # Default to original behaviour
@@ -384,18 +399,21 @@ class Pixaxe(ServiceBase):
                 result.add_section(ares)
                 file_name = "{}_appended_img_content".format(hashlib.sha256(additional_content).hexdigest()[0:10])
                 file_path = os.path.join(self.working_directory, file_name)
-                request.add_extracted(file_path, file_name, "Carved content found at end of image.",
-                                      safelist_interface=self.api_interface)
-                with open(file_path, 'wb') as unibu_file:
+                request.add_extracted(
+                    file_path, file_name, "Carved content found at end of image.", safelist_interface=self.api_interface
+                )
+                with open(file_path, "wb") as unibu_file:
                     unibu_file.write(additional_content)
 
         # Steganography modules
         try:
             img_info = ImageInfo(request.file_path, request, steg_section, self.working_directory, self.log)
-            self.log.debug(f'Pixel Count: {img_info.pixel_count}')
-            if img_info.pixel_count > 100 and \
-                    img_info.pixel_count < self.config.get('max_pixel_count', 100000) or \
-                    request.deep_scan:
+            self.log.debug(f"Pixel Count: {img_info.pixel_count}")
+            if (
+                img_info.pixel_count > 100
+                and img_info.pixel_count < self.config.get("max_pixel_count", 100000)
+                or request.deep_scan
+            ):
                 img_info.decloak()
 
             if steg_section.body or steg_section.subsections:
